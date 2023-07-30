75ad0e0659984495bee80c44b9a9d3268ed9e419
Vipin Saini <vipin.saini@jhavtech.com>
2023-07-30 11:54:38 +0530

    Initial Commit

---
+++ NavigationHeroAnimation/NavigationHeroAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationHeroAnimationApp.swift
+//  NavigationHeroAnimation
+//
+//  Created by Vipin Saini on 30-07-2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationHeroAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
